d5fbd43871d3e5f275f4c5d2a574e9d3e8cb03aa
Josh MacDonald <josh.macdonald@gmail.com>
2016-06-24 16:30:25 -0700

    Exercise the user-defined transport Recorder

--- test/c++11/tracer_test.cc
+++ test/c++11/tracer_test.cc
@@ -3,34 +3,48 @@
 #include <thread>
 
 #include "tracer.h"
+#include "impl.h"
 
 using namespace lightstep;
 
 int main() {
-  Value val1 = 0.54321;
-  Value val2 = "pooh";
-  Values val3 = { val1, val2 };
-  Dictionary val4 = { std::make_pair("hello", val3),
-		      std::make_pair("whatever", val2) };
+  try {
+    Value val1 = 0.54321;
+    Value val2 = "pooh";
+    Values val3 = { val1, val2 };
+    Dictionary val4 = { std::make_pair("hello", val3),
+			std::make_pair("whatever", val2) };
 
-  StartSpanOptions sopts;
-  sopts.operation_name = "span/test";
-  sopts.tags = std::move(val4);
+    StartSpanOptions sopts;
+    sopts.operation_name = "span/test";
+    sopts.tags = std::move(val4);
 
-  auto span = Tracer::Global().StartSpanWithOptions(sopts);
-  span.Finish();
-
-  TracerOptions topts;
-  topts.access_token = "bfcebc4e1fa7e66d5502a4af87ae854f";
-  topts.collector_host = "collector.lightstep.com";
-  topts.collector_port = 9997;
-  topts.collector_encryption = "tls";
-
-  Tracer::InitGlobal(NewTracer(topts));
-
-  while (true) {
-    span = Tracer::Global().StartSpanWithOptions(sopts);
+    auto span = Tracer::Global().StartSpanWithOptions(sopts);
     span.Finish();
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    TracerOptions topts;
+    // TODO these are not used b/c user-defined transport below.
+    // topts.access_token = "bfcebc4e1fa7e66d5502a4af87ae854f";
+    // topts.collector_host = "collector.lightstep.com";
+    // topts.collector_port = 9997;
+    // topts.collector_encryption = "tls";
+
+    topts.recorder_factory = [](const TracerImpl& tracer) {
+      UserDefinedTransportOptions tropts;
+      return lightstep::NewUserDefinedTransport(tracer, tropts);
+    };
+
+    Tracer::InitGlobal(NewTracer(topts));
+
+    for (int i = 0; i < 10; i++) {
+      span = Tracer::Global().StartSpanWithOptions(sopts);
+      span.Finish();
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    Tracer::Global().impl()->Flush();
+  } catch (std::exception &e) {
+    std::cerr << "Exception! " << e.what() << std::endl;
   }
+  return 0;
 }
