227169cca504eb221537de4c01cf8409e76c498d
Josh MacDonald <josh.macdonald@gmail.com>
2017-02-24 15:50:47 -0800

    Update test

--- test/c++11/flushproto_test.cc
+++ test/c++11/flushproto_test.cc
@@ -76,6 +76,44 @@ void check_test_context_against(const lightstep::SpanContext& got, const lightst
     }
 }
 
+static std::string base64_encode(const std::string &in) {
+
+    std::string out;
+
+    int val=0, valb=-6;
+    for (unsigned char c : in) {
+        val = (val<<8) + c;
+        valb += 8;
+        while (valb>=0) {
+            out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[(val>>valb)&0x3F]);
+            valb-=6;
+        }
+    }
+    if (valb>-6) out.push_back("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[((val<<8)>>(valb+8))&0x3F]);
+    while (out.size()%4) out.push_back('=');
+    return out;
+}
+
+static std::string base64_decode(const std::string &in) {
+
+    std::string out;
+
+    std::vector<int> T(256,-1);
+    for (int i=0; i<64; i++) T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i; 
+
+    int val=0, valb=-8;
+    for (unsigned char c : in) {
+        if (T[c] == -1) break;
+        val = (val<<6) + T[c];
+        valb += 6;
+        if (valb>=0) {
+            out.push_back(char((val>>valb)&0xFF));
+            valb-=8;
+        }
+    }
+    return out;
+}
+
 int main() {
   try {
     check_tracer_names();
@@ -111,7 +149,8 @@ int main() {
     }
 
     std::vector<std::pair<std::string, std::string>> headers;
-    lightstep::envoy::CarrierStruct envoystruct;
+
+    lightstep::envoy::EnvoyCarrier envoystruct;
     
     if (!lightstep::Tracer::Global().Inject(parent,
 					    lightstep::CarrierFormat::HTTPHeadersCarrier,
@@ -123,6 +162,10 @@ int main() {
 					    lightstep::envoy::ProtoWriter(&envoystruct))) {
       throw error("inject (envoy) failed");
     }
+
+    std::string data;
+    envoystruct.SerializeToString(&data);
+    std::cerr << "ENCODED: " << base64_encode(data) << std::endl;
 
     lightstep::SpanContext extracted_text =
       lightstep::Tracer::Global().Extract(lightstep::CarrierFormat::HTTPHeadersCarrier,
