4fe82f8f9735aaa0a8e2e5a10dbedf1c502ef65a
Ryan Burn <ryan.burn@gmail.com>
2017-06-14 11:50:31 -0400

    Set up version header file.

--- c++11/src/default_tags.h
+++ c++11/src/default_tags.h
@@ -1,7 +1,12 @@
-#include <utility>
+#include <lightstep/version.h>
 #include <opentracing/value.h>
+#include <opentracing/version.h>
+#include <utility>
 
 namespace lightstep {
 const std::pair<const char*, opentracing::Value> default_tags[] = {
-    {"lightstep.tracer_platform", "c++11"}, {"lightstep.tracer_version", ""}};
-} // namespace lightstep
+    {"lightstep.tracer_platform", "c++"},
+    {"lightstep.tracer_platform_version", __cplusplus},
+    {"lightstep.tracer_version", LIGHTSTEP_VERSION},
+    {"lightstep.opentracing_version", OPENTRACING_VERSION}};
+}  // namespace lightstep
