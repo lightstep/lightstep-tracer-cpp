1fa3376a39f5c67af71b90a0d9ef8dd9ff7a8d03
rnburn <ryan.burn@gmail.com>
2017-08-29 13:06:15 -0400

    Add missing header.

--- c++11/src/utility.cpp
+++ c++11/src/utility.cpp
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <sstream>
 #include <stdexcept>
+#include <random>
 
 namespace lightstep {
 //------------------------------------------------------------------------------
