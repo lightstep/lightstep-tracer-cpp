00869b13bd6c4997731cd9604b077075011736f2
Ryan Burn <ryan.burn@gmail.com>
2018-03-30 17:13:16 -0400

    Set up ASAN and TSAN coverage.

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -5,10 +5,24 @@ set -e
 [ -z "${SRC_DIR}" ] && export SRC_DIR="`pwd`"
 [ -z "${BUILD_DIR}" ] && export BUILD_DIR="`mktemp -d`"
 
-if [[ "$1" == "cmake.test" ]]; then
+if [[ "$1" == "cmake.asan" ]]; then
   cd "${BUILD_DIR}"
-  cmake -DCMAKE_BUILD_TYPE=Debug "${SRC_DIR}"
+  cmake -DCMAKE_BUILD_TYPE=Debug  \
+        -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -fsanitize=address"  \
+        -DCMAKE_SHARED_LINKER_FLAGS="-fno-omit-frame-pointer -fsanitize=address" \
+        -DCMAKE_EXE_LINKER_FLAGS="-fno-omit-frame-pointer -fsanitize=address" \
+        "${SRC_DIR}"
   make
+  make test
+  exit 0
+elif [[ "$1" == "cmake.tsan" ]]; then
+  cd "${BUILD_DIR}"
+  cmake -DCMAKE_BUILD_TYPE=Debug  \
+        -DCMAKE_CXX_FLAGS="-fno-omit-frame-pointer -fsanitize=thread"  \
+        -DCMAKE_SHARED_LINKER_FLAGS="-fno-omit-frame-pointer -fsanitize=thread" \
+        -DCMAKE_EXE_LINKER_FLAGS="-fno-omit-frame-pointer -fsanitize=thread" \
+        "${SRC_DIR}"
+  make VERBOSE=1
   make test
   exit 0
 fi
