eaffa556c803e7fd4eab7d4925d7942ec8b86840
Ryan Burn <ryan.burn@gmail.com>
2018-04-03 11:46:02 -0400

    Fix test scripts.

--- ci/install_protobuf.sh
+++ ci/install_protobuf.sh
@@ -5,6 +5,7 @@ set -e
 apt-get update 
 apt-get install --no-install-recommends --no-install-suggests -y \
          curl \
+         libprotobuf-dev \
          unzip
 
 # Make sure you grab the latest version
@@ -15,4 +16,4 @@ curl -OL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.
 unzip protoc-3.2.0-linux-x86_64.zip -d protoc3
 
 # Move only protoc* to /usr/bin/
-sudo mv protoc3/bin/protoc /usr/bin/protoc
+mv protoc3/bin/protoc /usr/bin/protoc
