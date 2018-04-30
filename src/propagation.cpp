bf2c12a69072b2368add5459e4b8bb6f88843872
Ryan Burn <ryan.burn@gmail.com>
2018-04-30 12:40:49 -0400

    Support multikey-single key context interoperability.

--- src/propagation.cpp
+++ src/propagation.cpp
@@ -341,12 +341,18 @@ static opentracing::expected<bool> ExtractSpanContext(
     uint64_t& span_id, std::unordered_map<std::string, std::string>& baggage,
     KeyCompare key_compare) {
   if (propagation_options.use_single_key) {
-    return ExtractSpanContextSingleKey(carrier, trace_id, span_id, baggage,
-                                       key_compare);
-  } else {
-    return ExtractSpanContextMultiKey(carrier, trace_id, span_id, baggage,
-                                      key_compare);
+    auto span_context_maybe = ExtractSpanContextSingleKey(
+        carrier, trace_id, span_id, baggage, key_compare);
+
+    // If no span context was found, fall back to multikey extraction so as to
+    // support interoperability with other tracers.
+    if (!span_context_maybe || *span_context_maybe) {
+      return span_context_maybe;
+    }
   }
+
+  return ExtractSpanContextMultiKey(carrier, trace_id, span_id, baggage,
+                                    key_compare);
 }
 
 opentracing::expected<bool> ExtractSpanContext(
