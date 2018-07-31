75a616ff28a2da2c530887534c294d2670808aa5
Ryan Burn <ryan.burn@gmail.com>
2018-07-31 14:16:44 -0700

    Generate coverage information.

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -53,6 +53,10 @@ elif [[ "$1" == "coverage" ]]; then
     "$SRC_DIR"
   make VERBOSE=1
   make test
+  cd build/CMakeFiles/lightstep_tracer.dir/src
+  gcovr -r "$SRC_DIR" . --html --html-details -o coverage.html
+  mkdir /coverage
+  cp *.html /coverage/
   exit 0
 elif [[ "$1" == "cmake.clang-tidy" ]]; then
   cd "${BUILD_DIR}"
