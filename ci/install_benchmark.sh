e6d8f5c791ff5bf51e4fa1fb38c5db8b35b4aa41
Ryan Burn <ryan.burn@gmail.com>
2018-08-17 10:26:20 -0700

    Fix install_benchmark.sh

--- ci/install_benchmark.sh
+++ ci/install_benchmark.sh
@@ -3,8 +3,8 @@
 set -e
 
 BENCHMARK_VERSION=v1.4.1
-git clone -b $BENCHMARK_VERSION https://github.com/google/benchmark.git
-cd benchmark
+git clone -b $BENCHMARK_VERSION https://github.com/google/benchmark.git google-benchmark
+cd google google-benchmark
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE
