323e1c87f1c0e27e4f4433f4e60f95b1a2bf31e2
Ryan Burn <ryan.burn@gmail.com>
2018-08-21 14:30:14 -0700

    Remove call to Clear.

--- src/lightstep_span.cpp
+++ src/lightstep_span.cpp
@@ -71,7 +71,6 @@ static bool SetSpanReference(
                     const opentracing::SpanContext*>& reference,
     BaggageMap& baggage, collector::Reference& collector_reference,
     bool& sampled) {
-  collector_reference.Clear();
   switch (reference.first) {
     case opentracing::SpanReferenceType::ChildOfRef:
       collector_reference.set_relationship(collector::Reference::CHILD_OF);
