fdd84fad40e958fc2089cdb744eb5501b77eddb4
Ryan Burn <ryan.burn@gmail.com>
2019-01-15 23:17:17 -0800

    Fix clang-tidy error #2.

--- cmake/Modules/LightStepClangTidy.cmake
+++ cmake/Modules/LightStepClangTidy.cmake
@@ -15,6 +15,7 @@ else()
 -hicpp-vararg,\
 -hicpp-signed-bitwise,\
 -hicpp-no-array-decay,\
+-hicpp-member-init,\
 -cppcoreguidelines-owning-memory,\
 -cppcoreguidelines-pro-type-member-init,\
 -cppcoreguidelines-pro-type-reinterpret-cast,\
