50f16f9ceb4481ce87ceacf85347d447e02c3bc8
Ryan Burn <ryan.burn@gmail.com>
2019-03-03 15:40:30 -0800

    Add a handler for streaming report requests.

--- test/mock_satellite/satellitehandler.go
+++ test/mock_satellite/satellitehandler.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+    "io"
+    "bufio"
     "bytes"
     "log"
     "net/http"
@@ -29,6 +31,15 @@ func NewSatelliteHandler(reportChannel chan *collectorpb.ReportRequest) *Satelli
   }
 }
 
+func sendResponse(responseWriter http.ResponseWriter, response *collectorpb.ReportResponse) {
+  response.TransmitTimestamp = getCurrentTime()
+  serializedResponse, err := proto.Marshal(response)
+  if err != nil {
+    log.Fatalf("Failed to marshal ReportResponse: %s\n", err.Error())
+  }
+  responseWriter.Write(serializedResponse)
+}
+
 func (handler *SatelliteHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
   response := &collectorpb.ReportResponse{
     ReceiveTimestamp: getCurrentTime(),
@@ -40,11 +51,35 @@ func (handler *SatelliteHandler) ServeHTTP(responseWriter http.ResponseWriter, r
   if err != nil {
     log.Fatalf("Failed to unmarshal ReportRequest: %s\n", err.Error())
   }
-  response.TransmitTimestamp = getCurrentTime()
-  serializedResponse, err := proto.Marshal(response)
-  if err != nil {
-    log.Fatalf("Failed to marshal ReportResponse: %s\n", err.Error())
-  }
-  responseWriter.Write(serializedResponse)
+  sendResponse(responseWriter, response)
   handler.reportChannel <- report
+}
+
+func (handler* SatelliteHandler) serveStreamingHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+  response := &collectorpb.ReportResponse{
+    ReceiveTimestamp: getCurrentTime(),
+  }
+  reader := bufio.NewReader(request.Body)
+  reportRequest, err := ReadStreamHeader(reader)
+  if err != nil {
+    log.Fatalf("ReadStreamHeader failed: %s\n", err.Error())
+  }
+  handler.reportChannel <- reportRequest
+
+  // Nulify InteralMetrics so that we can reuse the report request without reporting metrics multiple times
+  reportRequest.InternalMetrics = nil
+  reportRequest.Spans = make([]*collectorpb.Span, 1)
+
+  for {
+    span, err := ReadSpan(reader)
+    if err == io.EOF {
+      break
+    }
+    if err != nil {
+      log.Fatalf("ReadSpan failed: %s\n", err.Error())
+    }
+    reportRequest.Spans[0] = span
+    handler.reportChannel <- reportRequest
+  }
+  sendResponse(responseWriter, response)
 }
