84e513a27893ba926987cbfe8a6e144b1bd22359
Ryan Burn <ryan.burn@gmail.com>
2019-03-06 15:16:34 -0800

    Add additional coverage for Write.

--- test/network/vector_write_test.cpp
+++ test/network/vector_write_test.cpp
@@ -1,5 +1,7 @@
 #include "network/vector_write.h"
 
+#include <vector>
+
 #include "3rd_party/catch2/catch.hpp"
 #include "network/socket.h"
 #include "test/echo_server/echo_server_handle.h"
@@ -27,5 +29,36 @@ TEST_CASE("VectorWrite") {
     socket = Socket{};
     REQUIRE(result == std::make_tuple(2, 0, 0));
     REQUIRE(echo_server.data() == "abc123xyz");
+  }
+
+  SECTION(
+      "On a non-blocking socket, the vectorized write returns immediately with "
+      "a zero tuple if the socket would block.") {
+    socket.SetNonblocking();
+    std::vector<char> big_array(100000, ' ');
+    int i = 0;
+    int max_iterations = 1000;
+    for (; i < max_iterations; ++i) {
+      FragmentInputStream<4> fragments{{static_cast<void*>(big_array.data()),
+                                        static_cast<int>(big_array.size())},
+                                       {static_cast<void*>(big_array.data()),
+                                        static_cast<int>(big_array.size())},
+                                       {static_cast<void*>(big_array.data()),
+                                        static_cast<int>(big_array.size())},
+                                       {static_cast<void*>(big_array.data()),
+                                        static_cast<int>(big_array.size())}};
+      auto result = Write(socket.file_descriptor(), {&fragments});
+      if (result == std::make_tuple(0, 0, 0)) {
+        break;
+      }
+    }
+    REQUIRE(i < max_iterations);
+  }
+
+  SECTION("Write throws an exception when there's an error.") {
+    socket.SetNonblocking();
+    auto file_descriptor = socket.file_descriptor();
+    socket = Socket{};
+    REQUIRE_THROWS(Write(file_descriptor, {&fragments1, &fragments2}));
   }
 }
