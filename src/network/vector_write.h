73d61908f5716a8bb1ca43dfaa29f753a0576940
Ryan Burn <ryan.burn@gmail.com>
2019-03-06 23:19:13 -0800

    Fix typo.

--- src/network/vector_write.h
+++ src/network/vector_write.h
@@ -6,7 +6,7 @@
 
 namespace lightstep {
 /**
- * Uses the writev systme call to send fragments over a given socket.
+ * Uses the writev system call to send fragments over a given socket.
  * @param socket the file descriptor of the socket.
  * @param fragment_sets the list of fragments to send.
  * @return a tuple identifying the position of the last byte send.
