01908fe330ed0b12d62422deebe031940b7a10fb
Ryan Burn <ryan.burn@gmail.com>
2019-03-14 17:05:51 -0700

    Add test coverage for metrics.

--- test/recorder/stream_recorder/connection_stream_test.cpp
+++ test/recorder/stream_recorder/connection_stream_test.cpp
@@ -1,5 +1,7 @@
 #include "recorder/stream_recorder/connection_stream.h"
 
+#include <string>
+
 #include "3rd_party/catch2/catch.hpp"
 #include "recorder/stream_recorder/span_stream.h"
 #include "recorder/stream_recorder/utility.h"
@@ -15,6 +17,20 @@ static std::string ToString(
   return result;
 }
 
+collector::ReportRequest ParseStreamHeader(const std::string& s) {
+  collector::ReportRequest result;
+  auto body_start = s.find("\r\n\r\n");
+  REQUIRE(body_start != std::string::npos);
+  body_start += 4;
+  auto chunk_start = s.find("\r\n", body_start);
+  REQUIRE(chunk_start != std::string::npos);
+  auto chunk_size =
+      std::stoi(s.substr(body_start, body_start - chunk_start), 0, 16);
+  chunk_start += 2;
+  REQUIRE(result.ParseFromString(s.substr(chunk_start, chunk_size)));
+  return result;
+}
+
 TEST_CASE("ConnectionStream") {
   LightStepTracerOptions tracer_options;
   ChunkCircularBuffer span_buffer{1000};
@@ -24,7 +40,9 @@ TEST_CASE("ConnectionStream") {
   StreamRecorderMetrics metrics;
   auto host_header_fragment = MakeFragment("Host:abc\r\n");
   ConnectionStream connection_stream{
-      host_header_fragment, MakeFragment(header_common_fragment.c_str()),
+      host_header_fragment,
+      Fragment{static_cast<void*>(&header_common_fragment[0]),
+               static_cast<int>(header_common_fragment.size())},
       metrics, span_stream};
   std::string contents;
   REQUIRE(connection_stream.Flush(
@@ -78,10 +96,46 @@ TEST_CASE("ConnectionStream") {
     REQUIRE(connection_stream.completed());
   }
 
-  SECTION("The ConnectionStream consumes metrics.") {
+  SECTION(
+      "The ConnectionStream consumes metrics and sends in the stream header.") {
     metrics.num_dropped_spans += 3;
-    REQUIRE(metrics.num_dropped_spans == 3);
     connection_stream.Reset();
+    REQUIRE(metrics.num_dropped_spans == 0);
+    REQUIRE(connection_stream.Flush(
+        [&contents](
+            std::initializer_list<FragmentInputStream*> fragment_streams) {
+          contents = ToString(fragment_streams);
+          return Consume(fragment_streams, static_cast<int>(contents.size()));
+        }));
+    auto report_request = ParseStreamHeader(contents);
+    auto& counts = report_request.internal_metrics().counts();
+    REQUIRE(counts.size() == 1);
+    REQUIRE(counts[0].int_value() == 3);
+  }
+
+  SECTION(
+      "ConnectionStream adds metrics back if it fails to send the stream "
+      "header.") {
+    metrics.num_dropped_spans += 3;
+    connection_stream.Reset();
+    REQUIRE(metrics.num_dropped_spans == 0);
+    connection_stream.Flush(
+        [](std::initializer_list<FragmentInputStream*> fragment_streams) {
+          auto contents = ToString(fragment_streams);
+          return Consume(fragment_streams,
+                         static_cast<int>(contents.size()) - 1);
+        });
+    connection_stream.Reset();
+    REQUIRE(connection_stream.Flush(
+        [&contents](
+            std::initializer_list<FragmentInputStream*> fragment_streams) {
+          contents = ToString(fragment_streams);
+          return Consume(fragment_streams, static_cast<int>(contents.size()));
+        }));
+    auto report_request = ParseStreamHeader(contents);
+    auto& counts = report_request.internal_metrics().counts();
+    REQUIRE(counts.size() == 1);
+    REQUIRE(counts[0].int_value() == 3);
     REQUIRE(metrics.num_dropped_spans == 0);
   }
 }
