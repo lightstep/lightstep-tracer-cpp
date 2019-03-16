11dce8c70368acfa280aeb8096ec4471c27ba805
Ryan Burn <ryan.burn@gmail.com>
2019-03-15 23:41:21 -0700

    Remove magic numbers.

--- src/common/protobuf.cpp
+++ src/common/protobuf.cpp
@@ -1,13 +1,25 @@
 #include "common/protobuf.h"
 
 namespace lightstep {
+static const uint32_t WireTypeLengthDelimited = 2;
+static const int ProtoFieldShift = 3;
+
+//--------------------------------------------------------------------------------------------------
+// ComputeStreamMessageKey
+//--------------------------------------------------------------------------------------------------
+static uint32_t ComputeStreamMessageKey(uint32_t field, uint32_t wire_type) {
+  // See description of encoding process at
+  // https://developers.google.com/protocol-buffers/docs/encoding
+  return (field << ProtoFieldShift) | wire_type;
+}
+
 //--------------------------------------------------------------------------------------------------
 // WriteEmbeddedMessage
 //--------------------------------------------------------------------------------------------------
 void WriteEmbeddedMessage(google::protobuf::io::CodedOutputStream& stream,
                           uint32_t field,
                           const google::protobuf::Message& message) {
-  stream.WriteVarint32((field << 3) | 2);
+  stream.WriteVarint32(ComputeStreamMessageKey(field, WireTypeLengthDelimited));
   stream.WriteVarint64(message.ByteSizeLong());
   message.SerializeWithCachedSizes(&stream);
 }
@@ -17,8 +29,8 @@ void WriteEmbeddedMessage(google::protobuf::io::CodedOutputStream& stream,
 //--------------------------------------------------------------------------------------------------
 size_t ComputeEmbeddedMessageSerializationSize(
     uint32_t field, const google::protobuf::Message& message) {
-  size_t result =
-      google::protobuf::io::CodedOutputStream::VarintSize32((field << 3) | 2);
+  size_t result = google::protobuf::io::CodedOutputStream::VarintSize32(
+      ComputeStreamMessageKey(field, WireTypeLengthDelimited));
   auto message_size = message.ByteSizeLong();
   result +=
       google::protobuf::io::CodedOutputStream::VarintSize64(message_size) +
