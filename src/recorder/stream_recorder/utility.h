2c9a439818fdf5fc0b74af673acd77d0e769ed02
Ryan Burn <ryan.burn@gmail.com>
2019-03-21 22:15:18 -0700

    Document Contains.

--- src/recorder/stream_recorder/utility.h
+++ src/recorder/stream_recorder/utility.h
@@ -29,5 +29,12 @@ SeparateEndpoints(
 std::string WriteStreamHeaderCommonFragment(
     const LightStepTracerOptions& tracer_options, uint64_t reporter_id);
 
+/**
+ * Determines if a given range contains a pointer.
+ * @param data the start of the range
+ * @param size the size of the range
+ * @param ptr to pointer to check for
+ * @return true if ptr is contained in [data, data+size)
+ */
 bool Contains(const char* data, size_t size, const char* ptr) noexcept;
 }  // namespace lightstep
