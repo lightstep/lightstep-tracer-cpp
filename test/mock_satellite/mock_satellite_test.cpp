07782c2aee1c3496b104308199b7921f3d00149a
Ryan Burn <ryan.burn@gmail.com>
2019-04-02 13:56:18 -0700

    Make requests compatible.

--- test/mock_satellite/mock_satellite_test.cpp
+++ test/mock_satellite/mock_satellite_test.cpp
@@ -46,7 +46,7 @@ TEST_CASE("MockSatellite") {
     span1.mutable_span_context()->set_span_id(1);
     *report.mutable_spans()->Add() = span1;
     collector::ReportResponse response;
-    connection.Post("/report", report, response);
+    connection.Post("/api/v2", report, response);
     REQUIRE(mock_satellite.spans().size() == 1);
   }
 
