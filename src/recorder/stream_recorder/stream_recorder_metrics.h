9af48cf414007e818a41bb6fb2c19e0cfbaa05a6
Ryan Burn <ryan.burn@gmail.com>
2019-04-27 21:02:18 -0700

    Document StreamRecorderMetrics.

--- src/recorder/stream_recorder/stream_recorder_metrics.h
+++ src/recorder/stream_recorder/stream_recorder_metrics.h
@@ -12,16 +12,38 @@ class StreamRecorderMetrics {
  public:
   explicit StreamRecorderMetrics(MetricsObserver& metrics_observer) noexcept;
 
+  /**
+   * Record dropped spans.
+   * @param num_spans the number of spans dropped.
+   */
   void OnSpansDropped(int num_spans) noexcept;
 
+  /**
+   * Record spans sent.
+   * @param num_spans the number of spans sent.
+   */
   void OnSpansSent(int num_spans) noexcept;
 
+  /**
+   * Record flushes.
+   */
   void OnFlush() noexcept;
 
+  /**
+   * Return then clear the dropped span counter.
+   * @return the dropped span count.
+   */
   int ConsumeDroppedSpans() noexcept;
 
+  /**
+   * Add spans back to the dropped span counter.
+   * @param num_spans the number of dropped spans to add back.
+   */
   void UnconsumeDroppedSpans(int num_spans) noexcept;
 
+  /**
+   * @return the dropped span count.
+   */
   int num_dropped_spans() const noexcept { return num_dropped_spans_; }
 
  private:
