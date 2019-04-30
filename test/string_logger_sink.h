ad99b0186e8bc9f30c4d0eec30e844d97cc0463a
Ryan Burn <ryan.burn@gmail.com>
2019-04-29 22:17:11 -0700

    Document StringLoggerSink.

--- test/string_logger_sink.h
+++ test/string_logger_sink.h
@@ -5,11 +5,22 @@
 #include <sstream>
 
 namespace lightstep {
+/**
+ * A log sink that concatenates all records into a string.
+ */
 class StringLoggerSink {
  public:
+  /**
+   * Append the given log.
+   * @param log_level the level to log at
+   * @param message the log message
+   */
   void operator()(LogLevel log_level,
                   opentracing::string_view message) noexcept;
 
+  /**
+   * @return a string of concatenated log records.
+   */
   std::string contents();
 
  private:
