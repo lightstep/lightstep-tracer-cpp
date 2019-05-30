d75af7a032097feec59334225ca16548a0a4722e
Ryan <ryan.burn@gmail.com>
2019-05-30 11:46:11 -0700

    Support portably linking python module (#159)
    * Change linking for python module.
    
    * Support statically linking libstdc++

--- test/bridge/python/tracer_test.py
+++ test/bridge/python/tracer_test.py
@@ -6,7 +6,10 @@ import lightstep
 
 class TestTracer(unittest.TestCase):
     def test_report_span(self):
-        tracer = lightstep.Tracer(access_token='abc')
+        tracer = lightstep.Tracer(access_token='abc',
+                                  use_stream_recorder=True,
+                                  collector_plaintext=True,
+                                  satellite_endpoints=[{'host':'locahost', 'port':123}])
 
 
 if __name__ == '__main__':
