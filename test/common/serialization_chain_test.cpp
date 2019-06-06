b9c64e366914c8a8ee3f8ae0c3d86ce90aecc154
Ryan Burn <ryan.burn@gmail.com>
2019-06-06 14:27:51 -0700

    Add SerializationChain test coverage.

--- test/common/serialization_chain_test.cpp
+++ test/common/serialization_chain_test.cpp
@@ -22,7 +22,7 @@ TEST_CASE("SerializationChain") {
     REQUIRE(chain.num_fragments() == 0);
   }
 
-  SECTION("We can write a string smaller than the block size") {
+  SECTION("We can write a string smaller than the block size.") {
     stream->WriteString("abc");
     stream.reset();
     chain.AddChunkFraming();
@@ -30,7 +30,7 @@ TEST_CASE("SerializationChain") {
     REQUIRE(ToString(chain) == "3\r\nabc\r\n");
   }
 
-  SECTION("We can write strings larger than a single block") {
+  SECTION("We can write strings larger than a single block.") {
     std::string s(SerializationChain::BlockSize+1, 'X');
     stream->WriteString(s);
     stream.reset();
@@ -39,5 +39,23 @@ TEST_CASE("SerializationChain") {
     std::ostringstream oss;
     oss << std::hex << std::uppercase << s.size() << "\r\n" << s << "\r\n";
     REQUIRE(ToString(chain) == oss.str());
+  }
+
+  SECTION("We can seek to any byte in the fragment stream.") {
+    std::string s(SerializationChain::BlockSize+2, 'X');
+    stream->WriteString(s);
+    stream.reset();
+    chain.AddChunkFraming();
+    std::string serialization = [&] {
+      std::ostringstream oss;
+      oss << std::hex << std::uppercase << s.size() << "\r\n" << s << "\r\n";
+      return oss.str();
+    }();
+    for (size_t i = 1; i <= serialization.size(); ++i) {
+      SECTION("cosumption instance") {
+        Consume({&chain}, i);
+        REQUIRE(ToString(chain) == serialization.substr(i));
+      }
+    }
   }
 }
