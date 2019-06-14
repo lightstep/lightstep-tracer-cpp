0183918b2af375e10fc59e98c3c4ea55e0b64537
Ryan Burn <ryan.burn@gmail.com>
2019-06-14 12:04:27 -0700

    Fix argument validation.

--- test/bridge/python/tracer_test.py
+++ test/bridge/python/tracer_test.py
@@ -5,6 +5,16 @@ sys.path.append('bridge/python')
 import lightstep_native
 
 class TestTracer(unittest.TestCase):
+    def test_error_on_bad_config(self):
+        with self.assertRaises(Exception):
+            lightstep_native.Tracer()
+
+        with self.assertRaises(Exception):
+            lightstep_native.Tracer('abc')
+
+        with self.assertRaises(Exception):
+            lightstep_native.Tracer(xyz='abc')
+
     def test_report_span(self):
         tracer = lightstep_native.Tracer(access_token='abc',
                                          use_stream_recorder=True,
