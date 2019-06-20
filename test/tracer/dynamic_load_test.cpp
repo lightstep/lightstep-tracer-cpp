c582845b6b230e5254f5f8222634d46828af2554
Ryan Burn <ryan.burn@gmail.com>
2019-06-19 18:26:23 -0700

    Remove debugging cruft.

--- test/tracer/dynamic_load_test.cpp
+++ test/tracer/dynamic_load_test.cpp
@@ -7,29 +7,6 @@
 #include "test/utility.h"
 
 #include "3rd_party/catch2/catch.hpp"
-
-/////////////////////////////////////////////////////////
-#include <cstdio>
-#include <iostream>
-#include <memory>
-#include <stdexcept>
-#include <string>
-#include <array>
-
-std::string exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
-}
-/////////////////////////////////////////////////////////
-
 using namespace lightstep;
 
 TEST_CASE("dynamic_load") {
@@ -37,9 +14,6 @@ TEST_CASE("dynamic_load") {
 
   auto handle_maybe = opentracing::DynamicallyLoadTracingLibrary(
       "./liblightstep_tracer_plugin.so", error_message);
-  INFO("TEST_TEMP_DIR: " << exec("echo $TEST_TMPDIR"));
-  INFO("pwd output: " << exec("pwd"));
-  INFO("ls output: " << exec("ls"));
   INFO(error_message);
   REQUIRE(error_message.empty());
   REQUIRE(handle_maybe);
@@ -52,6 +26,7 @@ TEST_CASE("dynamic_load") {
     ]
     )";
     auto tracer_maybe = tracer_factory.MakeTracer(config, error_message);
+    INFO(error_message);
     CHECK(!error_message.empty());
     CHECK(!tracer_maybe);
   }
