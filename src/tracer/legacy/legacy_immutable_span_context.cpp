2c45c484ddfab13bfc58552017a58c96339b2abd
Ryan Burn <ryan.burn@gmail.com>
2019-06-25 23:24:05 -0700

    s/LightStepImmutableSpanContext/LegacyImmutableSpanContext/

---
+++ src/tracer/legacy/legacy_immutable_span_context.cpp
@@ -0,0 +1,37 @@
+#include "tracer/legacy/legacy_immutable_span_context.h"
+
+namespace lightstep {
+//------------------------------------------------------------------------------
+// constructor
+//------------------------------------------------------------------------------
+LegacyImmutableSpanContext::LegacyImmutableSpanContext(
+    uint64_t trace_id, uint64_t span_id, bool sampled,
+    const std::unordered_map<std::string, std::string>& baggage)
+    : trace_id_{trace_id}, span_id_{span_id}, sampled_{sampled} {
+  for (auto& baggage_item : baggage) {
+    baggage_.insert(
+        BaggageMap::value_type(baggage_item.first, baggage_item.second));
+  }
+}
+
+LegacyImmutableSpanContext::LegacyImmutableSpanContext(
+    uint64_t trace_id, uint64_t span_id, bool sampled,
+    BaggageMap&& baggage) noexcept
+    : trace_id_{trace_id},
+      span_id_{span_id},
+      sampled_{sampled},
+      baggage_{std::move(baggage)} {}
+
+//------------------------------------------------------------------------------
+// ForeachBaggageItem
+//------------------------------------------------------------------------------
+void LegacyImmutableSpanContext::ForeachBaggageItem(
+    std::function<bool(const std::string& key, const std::string& value)> f)
+    const {
+  for (const auto& baggage_item : baggage_) {
+    if (!f(baggage_item.first, baggage_item.second)) {
+      return;
+    }
+  }
+}
+}  // namespace lightstep
