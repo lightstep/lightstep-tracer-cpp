c380437cdca053d513ad0846144fa30e1436ac47
Ryan Burn <ryan.burn@gmail.com>
2019-07-27 01:40:04 -0500

    Try different varint serialization #11

--- src/common/direct_coded_output_stream.h
+++ src/common/direct_coded_output_stream.h
@@ -102,7 +102,31 @@ class DirectCodedOutputStream {
   }
 #endif
 
-  void WriteBigVarint64(uint64_t x) noexcept;
+  void WriteBigVarint64(uint64_t x) noexcept {
+    static const uint64_t pow_2_56 = (1ull << 56);
+    if (__builtin_expect(x < pow_2_56, 0)) {
+      WriteVarint64(x);
+      return;
+    }
+#define DO_ITERATION                                           \
+  {                                                            \
+    *data_++ = static_cast<google::protobuf::uint8>(x | 0x80); \
+    x >>= 7;                                                   \
+  }
+    DO_ITERATION;  // 1
+    DO_ITERATION;  // 2
+    DO_ITERATION;  // 3
+    DO_ITERATION;  // 4
+    DO_ITERATION;  // 5
+    DO_ITERATION;  // 6
+    DO_ITERATION;  // 7
+    DO_ITERATION;  // 8
+    if (x >= 0x80) {
+      DO_ITERATION;
+    }
+    *data_++ = static_cast<google::protobuf::uint8>(x);
+#undef DO_ITERATION
+  }
 
  private:
   google::protobuf::uint8* data_;
