542a23c183451af6005cd4a30d99730b317617e9
Ryan Burn <ryan.burn@gmail.com>
2019-07-30 13:14:59 -0700

    Try installing additional windows dependencies.

--- ci/setup_windows_build_environment.sh
+++ ci/setup_windows_build_environment.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+set -e
 cd $HOMEDRIVE
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.bat
 ./vcpkg integrate install
 ./vcpkg install libevent:x64-windows-static
+./vcpkg install protobuf:x64-windows-static
+./vcpkg install opentracing:x64-windows-static
+./vcpkg install cares:x64-windows-static
