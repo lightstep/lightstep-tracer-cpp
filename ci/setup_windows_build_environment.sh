155fa7d83b7bfb3778840e9d0db5c9dc9642cc13
Ryan Burn <ryan.burn@gmail.com>
2019-07-30 16:04:10 -0700

    Try curl again.

--- ci/setup_windows_build_environment.sh
+++ ci/setup_windows_build_environment.sh
@@ -4,7 +4,7 @@ set -e
 cd $HOMEDRIVE
 
 # install cmake
-Invoke-WebRequest https://github.com/Kitware/CMake/releases/download/v3.15.1/cmake-3.15.1-win64-x64.zip -OutFile cmake-3.15.1-win64-x64.zip
+curl -O https://github.com/Kitware/CMake/releases/download/v3.15.1/cmake-3.15.1-win64-x64.zip
 
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
