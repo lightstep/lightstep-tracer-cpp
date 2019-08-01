ac8abcc534fd6397525f0e10cfc4c01497e7bb43
Ryan Burn <ryan.burn@gmail.com>
2019-08-01 16:28:30 -0700

    Generate additional data with profiling.

--- bazel/profile_benchmark.sh
+++ bazel/profile_benchmark.sh
@@ -10,15 +10,14 @@ TEMPDIR=$(mktemp -d)
 echo $TEMPDIR
 pushd $TEMPDIR
 for FILTER in $BENCHMARKS; do
+  FILTER_PRIME=$(echo $FILTER | tr / -)
   echo "Generate profile for $FILTER"
   valgrind --tool=callgrind $BENCHMARK --benchmark_filter=$FILTER$
   echo "Convert $FILTER to image"
-  gprof2dot --format=callgrind --output=out.dot callgrind.out.*
-  FILTER_PRIME=$(echo $FILTER | tr / -)
-  dot -Tpng out.dot -o "$BENCHMARK_NAME-$FILTER_PRIME.png"
-  rm callgrind.out.*
-  rm *.dot
-  tar zcf $(basename $OUTPUT) *.png
+  gprof2dot --format=callgrind --output=$BENCHMARK_NAME-$FILTER_PRIME.dot callgrind.out.[0-9]*
+  dot -Tpng $BENCHMARK_NAME-$FILTER_PRIME.dot -o "$BENCHMARK_NAME-$FILTER_PRIME.png"
+  mv callgrind.out.[0-9]* callgrind.out.$BENCHMARK_NAME-$FILTER_PRIME
+  tar zcf $(basename $OUTPUT) *.png *.dot callgrind.out.*
 done
 popd
 mv $TEMPDIR/$(basename $OUTPUT) $OUTPUT
