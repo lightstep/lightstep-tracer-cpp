294134467d13224d7fcf8f388285d5b55aa809f1
Ryan Burn <ryan.burn@gmail.com>
2019-08-02 13:01:03 -0700

    Set up vcpkg on windows.

--- ci/setup_windows_build_environment.sh
+++ ci/setup_windows_build_environment.sh
@@ -4,13 +4,13 @@ set -e
 cd $HOMEDRIVE
 
 # install cmake
-curl -O https://github.com/Kitware/CMake/releases/download/v3.15.1/cmake-3.15.1-win64-x64.zip
+# curl -O https://github.com/Kitware/CMake/releases/download/v3.15.1/cmake-3.15.1-win64-x64.zip
 
 # intall visual studio
-curl -O https://aka.ms/vs/15/release/vs_community.exe
-./vs_community.exe --installPath C:\minVS \
-  --add Microsoft.VisualStudio.Workload.CoreEditor \
-  --passive --norestart
+# curl -O https://aka.ms/vs/15/release/vs_community.exe
+# ./vs_community.exe --installPath C:\minVS \
+#   --add Microsoft.VisualStudio.Workload.CoreEditor \
+#   --passive --norestart
 
 git clone https://github.com/Microsoft/vcpkg.git
 cd vcpkg
