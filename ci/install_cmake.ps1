2813f34df873546f6d811437380cf115a09e8d57
Ryan Burn <ryan.burn@gmail.com>
2019-08-09 10:32:04 -0700

    Fix install script.

--- ci/install_cmake.ps1
+++ ci/install_cmake.ps1
@@ -1,7 +1,7 @@
 # See https://stackoverflow.com/a/9949909/4447365
 $ErrorActionPreference = "Stop"
 
-$CMAKE_VERSION=3.15.2
+$CMAKE_VERSION="3.15.2"
 $CWD=(Get-Item -Path ".\").FullName
 (new-object System.Net.WebClient). `
    DownloadFile("https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-win64-x64.zip", `
