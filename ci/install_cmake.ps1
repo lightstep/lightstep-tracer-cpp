189fee635c8d65c65883b3c20eb2008cfbe17cb1
Ryan Burn <ryan.burn@gmail.com>
2019-08-09 10:22:31 -0700

    Tweak install script.

--- ci/install_cmake.ps1
+++ ci/install_cmake.ps1
@@ -1,8 +1,9 @@
+# See https://stackoverflow.com/a/9949909/4447365
+$ErrorActionPreference = "Stop"
+
 $CMAKE_VERSION=3.15.2
 $CWD=(Get-Item -Path ".\").FullName
-(new-object System.Net.WebClient).`
-  DownloadFile('https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-win64-x64.zip', `
-               $CWD + '\cmake-$CMAKE_VERSION-win64-x64.zip')
+(new-object System.Net.WebClient).DownloadFile('https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-win64-x64.zip', $CWD + '\cmake-$CMAKE_VERSION-win64-x64.zip')
 
 unzip cmake-$CMAKE_VERSION-win64-x64.zip
 
