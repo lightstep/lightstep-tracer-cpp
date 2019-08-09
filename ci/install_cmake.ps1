bf103722bc0ff2bb3ad3b19eb12179495b69c8d2
Ryan Burn <ryan.burn@gmail.com>
2019-08-09 10:14:10 -0700

    Try installing cmake

---
+++ ci/install_cmake.ps1
@@ -0,0 +1,10 @@
+$CMAKE_VERSION=3.15.2
+$CWD=(Get-Item -Path ".\").FullName
+(new-object System.Net.WebClient).`
+  DownloadFile('https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION-win64-x64.zip', `
+               $CWD + '\cmake-$CMAKE_VERSION-win64-x64.zip')
+
+unzip cmake-$CMAKE_VERSION-win64-x64.zip
+
+$ENV:PATH="$ENV:PATH;$CWD\cmake-$CMAKE_VERSION-win64-x64\bin"
+cmake --help
