3da473256c052305e955f0fcb04741dbfa6325aa
Ryan Burn <ryan.burn@gmail.com>
2019-08-15 23:14:19 -0700

    Remove cruft.

--- ci/do_ci.sh
+++ ci/do_ci.sh
@@ -131,16 +131,16 @@ elif [[ "$1" == "plugin" ]]; then
     //test/mock_satellite:mock_satellite \
     //test/mock_satellite:mock_satellite_query \
     //test/tracer:span_probe \
-    //bridge/python:wheel.tgz
+    //bridge/python:wheels2.tgz
   mkdir -p /plugin
   cp bazel-bin/liblightstep_tracer_plugin.so /plugin
   cp bazel-bin/benchmark/tracer_upload_bench/tracer_upload_bench /plugin
   cp bazel-bin/test/mock_satellite/mock_satellite /plugin
   cp bazel-bin/test/mock_satellite/mock_satellite_query /plugin
   cp bazel-bin/test/tracer/span_probe /plugin
-  cp bazel-genfiles/bridge/python/wheel.tgz /
+  cp bazel-genfiles/bridge/python/wheels2.tgz /
   cd /
-  tar zxf wheel.tgz
+  tar zxf wheels2.tgz
   cp wheel/* plugin/
   exit 0
 elif [[ "$1" == "plugin.test" ]]; then
