bbf5c68d1a14ff0ad039a5fafc0803bdd7397303
Ryan Burn <ryan.burn@gmail.com>
2020-01-02 23:04:11 -0800

    Join trace-states from referenced spans

---
+++ src/tracer/utility.cpp
@@ -0,0 +1,14 @@
+#include "tracer/utility.h"
+
+namespace lightstep {
+void AppendTraceState(std::string& trace_state,
+                      opentracing::string_view key_values) {
+  if (trace_state.empty()) {
+    trace_state.assign(key_values.data(), key_values.size());
+    return;
+  }
+  trace_state.reserve(trace_state.size() + 1 + key_values.size());
+  trace_state.append(",");
+  trace_state.append(key_values.data(), key_values.size());
+}
+} // namespace lightstep
