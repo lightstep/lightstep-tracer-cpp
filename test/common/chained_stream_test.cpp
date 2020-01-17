2dcd8959ca68b5301e33831e28dcb88e092e2f01
Ryan Burn <ryan.burn@gmail.com>
2020-01-17 00:15:45 -0800

    Add test for ChainedStream

---
+++ test/common/chained_stream_test.cpp
@@ -0,0 +1,14 @@
+#include "common/chained_stream.h"
+
+#include <iomanip>
+#include <random>
+
+#include "test/utility.h"
+
+#include <google/protobuf/io/coded_stream.h>
+
+#include "3rd_party/catch2/catch.hpp"
+using namespace lightstep;
+
+TEST_CASE("ChainedStream") {
+}
