3554a50d5f87e72e9e0bb193220de56b9e7e4113
Ryan Burn <ryan.burn@gmail.com>
2020-01-27 13:40:17 -0800

    Fill out ReportRequest tests

--- test/recorder/serialization/report_request_test.cpp
+++ test/recorder/serialization/report_request_test.cpp
@@ -1,8 +1,13 @@
 #include "recorder/serialization/report_request.h"
 
+#include <array>
+
+#include "common/report_request_framing.h"
 #include "recorder/serialization/report_request_header.h"
 #include "3rd_party/catch2/catch.hpp"
 #include "lightstep-tracer-common/collector.pb.h"
+
+#include <google/protobuf/io/coded_stream.h>
 using namespace lightstep;
 
 static collector::ReportRequest ToProtobufReportRequest(
@@ -14,6 +19,25 @@ static collector::ReportRequest ToProtobufReportRequest(
     std::cerr << "Failed to parse report request\n";
     std::terminate();
   }
+  return result;
+}
+
+static std::unique_ptr<ChainedStream> ToSerialization(
+    const collector::Span& span) {
+  auto s = span.SerializeAsString();
+  std::array<char, ReportRequestSpansMaxHeaderSize> buffer;
+  auto header_size = WriteReportRequestSpansHeader(
+      buffer.data(), buffer.size(), static_cast<uint32_t>(s.size()));
+  s.insert(0, buffer.data() + (buffer.size() - header_size), header_size);
+
+  std::unique_ptr<ChainedStream> result{new ChainedStream{}};
+  std::unique_ptr<google::protobuf::io::CodedOutputStream> stream{
+      new google::protobuf::io::CodedOutputStream{result.get()}};
+  stream->WriteRaw(static_cast<const void*>(s.data()),
+                   static_cast<int>(s.size()));
+  stream.reset();
+  result->CloseOutput();
+
   return result;
 }
 
@@ -38,5 +62,29 @@ TEST_CASE("ReportRequest") {
         protobuf_report_request.internal_metrics().counts()[0];
     REQUIRE(dropped_span_count.name() == "spans.dropped");
     REQUIRE(dropped_span_count.int_value() == 42);
+  }
+
+  SECTION("We can add a span to the ReportRequest") {
+    collector::Span span;
+    span.mutable_span_context()->set_trace_id(1);
+    report_request.AddSpan(ToSerialization(span));
+    auto protobuf_report_request = ToProtobufReportRequest(report_request);
+    REQUIRE(protobuf_report_request.spans().size() == 1);
+    REQUIRE(protobuf_report_request.spans()[0].span_context().trace_id() == 1);
+  }
+
+  SECTION("We can add mulltiple spans to the ReportRequest") {
+    collector::Span span1;
+    span1.mutable_span_context()->set_trace_id(1);
+    report_request.AddSpan(ToSerialization(span1));
+
+    collector::Span span2;
+    span2.mutable_span_context()->set_trace_id(2);
+    report_request.AddSpan(ToSerialization(span2));
+
+    auto protobuf_report_request = ToProtobufReportRequest(report_request);
+    REQUIRE(protobuf_report_request.spans().size() == 2);
+    REQUIRE(protobuf_report_request.spans()[0].span_context().trace_id() == 1);
+    REQUIRE(protobuf_report_request.spans()[1].span_context().trace_id() == 2);
   }
 }
