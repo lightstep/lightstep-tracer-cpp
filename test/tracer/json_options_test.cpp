a0e17aa0b3171d75d64d7ebdd1d16beef3653baa
Ryan Burn <ryan.burn@gmail.com>
2020-05-20 09:47:30 -0700

    Add tags to configuration options

--- test/tracer/json_options_test.cpp
+++ test/tracer/json_options_test.cpp
@@ -6,7 +6,7 @@ using namespace lightstep;
 TEST_CASE("LightStepTracerFactory") {
   std::string error_message;
 
-  SECTION("We can specify propagation modes via a tracers json configuration") {
+  SECTION("We can specify propagation modes via a tracer's json configuration") {
     const char* config = R"({
       "component_name": "t",
       "propagation_modes": ["lightstep", "b3", "envoy", "trace_context"]
@@ -17,6 +17,21 @@ TEST_CASE("LightStepTracerFactory") {
         PropagationMode::lightstep, PropagationMode::b3, PropagationMode::envoy,
         PropagationMode::trace_context};
     REQUIRE(options_maybe->propagation_modes == expected_propagation_modes);
+  }
+
+  SECTION("We can specify string tags from a tracer's json configuration") {
+    const char* config = R"({
+      "component_name": "t",
+      "tags": {
+        "abc": "123",
+        "xyz": "456"
+      }
+    })";
+    auto options_maybe = MakeTracerOptions(config, error_message);
+    REQUIRE(options_maybe);
+    auto& options = *options_maybe;
+    REQUIRE(options.tags["abc"].get<std::string>() == "123");
+    REQUIRE(options.tags["xyz"].get<std::string>() == "456");
   }
 
   SECTION("MakeTracerOptions fails if there's an invalid propagation_mode") {
