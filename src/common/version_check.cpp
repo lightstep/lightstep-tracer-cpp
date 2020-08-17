f7d6483a034aa96d2125963b47cc408c79a31332
Ryan Burn <ryan.burn@gmail.com>
2020-08-16 17:06:38 -0700

    Back to passing

--- src/common/version_check.cpp
+++ src/common/version_check.cpp
@@ -1,4 +1,4 @@
 #include <opentracing/version.h>
 
-static_assert(OPENTRACING_ABI_VERSION[0] == '3',
+static_assert(OPENTRACING_ABI_VERSION[0] == '2',
     "Invalid version of OpenTracing. Please install a version using the v2 ABI.");
